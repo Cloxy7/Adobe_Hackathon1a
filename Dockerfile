574a675a2d0f472df8b6069cbc88ce025885811a
moyrsd <me22b120@smail.iitm.ac.in>
2025-07-28 19:50:44 +0530

    minor fixes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
-FROM --platform=linux/amd64 python:3.10-slim
+FROM python:3.10-slim
+
 WORKDIR /app
+
+# Copy requirements and install
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+
+COPY save_sbert_model.py .
+
+# Copy model folder early to a temp location
+RUN python save_sbert_model.py
+
+# Copy rest of the application
 COPY . .
-CMD ["python", "save_sbert_model.py"]
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from src.helpers.main_process import process_input_directory
 import time
 
+
 start = time.time() 
 
 
@@ -10,5 +11,6 @@ if __name__ == "__main__":
     OUTPUT_DIRECTORY = "output"
     process_input_directory(INPUT_DIRECTORY, OUTPUT_DIRECTORY)
     print("\nOperation completed successfully. Check the 'output' directory for results.")
+    time.sleep(10000000)
 end = time.time()
 print(f"Execution time: {end - start:.4f} seconds")
